c38d679e21f87e5bb645445e8da5eb1f44866ede
TheAndrey <andrey9506@yandex.ru>
2016-02-23 23:56:35 +0200

    BukkitEventFactory - добавлен EntityDamageByBlockEvent

--- src/theandrey/bukkit/event/BukkitEventFactory.java
+++ src/theandrey/bukkit/event/BukkitEventFactory.java
@@ -4,8 +4,13 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.ChunkPosition;
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 /**
@@ -25,6 +30,20 @@ public final class BukkitEventFactory {
 		Set<Player> brecipients = new HashSet<>();
 		for(EntityPlayer r : recipients) brecipients.add(BukkitEventUtils.getPlayer(r));
 		return new AsyncPlayerChatEvent(!Bukkit.isPrimaryThread(), bsender, message, brecipients);
+	}
+
+	/**
+	 * Создаёт эвент нанесения урона блоком Entity
+	 * @param blockpos Координаты блока
+	 * @param damaged Entity получивший урон
+	 * @param cause Причина
+	 * @param damage Урон
+	 * @return
+	 */
+	public static final EntityDamageByBlockEvent newEntityDamageByBlockEvent(ChunkPosition blockpos, net.minecraft.entity.Entity damaged, EntityDamageEvent.DamageCause cause, int damage) {
+		Entity bentity = BukkitEventUtils.getBukkitEntity(damaged);
+		Block block = bentity.getWorld().getBlockAt(blockpos.x, blockpos.y, blockpos.z);
+		return new EntityDamageByBlockEvent(block, bentity, cause, damage);
 	}
 
 }
