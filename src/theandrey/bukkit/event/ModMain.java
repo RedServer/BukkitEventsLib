3cb7320acf2f51fa24a47b63af44b75f5c0af063
TheAndrey <andrey9506@yandex.ru>
2017-05-17 14:26:20 +0200

    Версия 1.2

--- src/theandrey/bukkit/event/ModMain.java
+++ src/theandrey/bukkit/event/ModMain.java
@@ -18,7 +18,7 @@ public final class ModMain {
 
 	public static final String MOD_ID = "BukkitEventsLib";
 	public static final String MOD_NAME = "Bukkit Events Lib";
-	public static final String MOD_VERSION = "1.1";
+	public static final String MOD_VERSION = "1.2";
 	public static Logger logger;
 
 	@Mod.EventHandler
