de9f53b2d9d28ea6dd9abc738e4188b70de25527
TheAndrey <me@theandrey.name>
2021-08-16 23:55:50 +0200

    Удалён AbstractOwnerInfo
    Много ненужного

--- src/main/java/theandrey/bukkit/event/util/asm/ASMAccessor.java
+++ src/main/java/theandrey/bukkit/event/util/asm/ASMAccessor.java
@@ -6,7 +6,7 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
-import theandrey.bukkit.event.util.ReflectionHelper;
+import theandrey.bukkit.event.helper.ReflectionHelper;
 
 /**
  * Фабрика ASMAccessor
